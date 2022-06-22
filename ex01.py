bb4b2203a92d83ac0da2fa9912b7f647f050d16b
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-22 12:34:01 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,11 +1,11 @@
 #
 #entrada
-valor1 = int(input("Digite um valor: "))
+valor1 = int(input("Digite um valor: ")) # entrada
 
 # processamento e saída
-if valor1 % 2 == 0:
+if valor1 % 2 == 0: # se for par
     print(f"O valor {valor1} é par") # saída
-else:
+else: # se for impar
     print("O valor é impar")  # saída
 
 print("Fim do programa")
@@ -14,5 +14,5 @@ print("Fim do programa")
 print('-----------------------------------------------------')
 # extra 01
 # se for apenas para verificar o valor é impar, não precisa de else
-if not(valor1 % 2) == 0:
+if not(valor1 % 2) == 0: # se for impar
     print(f"O valor {valor1} é impar") # saída
