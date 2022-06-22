a9f244c02b5922ae92cb58bd33a716de4f594e3d
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-22 13:51:59 -0300

    Create ex04.py

---
+++ ex04.py
@@ -0,0 +1,45 @@
+#
+#entrada
+# quantidade de pessoas da cidade
+totalPessoas = int(input("Digite a quantidade de pessoas da cidade: "))
+# quantidade de pessoas que foram vacinadas com Coronavac
+quantCoronavac = int(input("Digite a quantidade de coronavírus: "))
+# quantidade de pessoas que foram vacinadas com Astrazeneca
+quantAstrazeneca = int(input("Digite a quantidade de astrazeneca: "))
+# quantidade de pessoas que foram vacinadas com Pfizer
+quantPfizer = int(input("Digite a quantidade de Pfizer: "))
+# quantidade de pessoas que foram vacinadas com janssen
+quantJanssen = int(input("Digite a quantidade de janssen: "))
+
+# processamento
+# quantidade de vacinados
+quantVacinados = quantCoronavac + quantAstrazeneca + quantPfizer + quantJanssen
+porcentagemVacinados = (quantVacinados * 100) / totalPessoas
+# calcula a porcentagem de pessoas que foram vacinadas com Coronavac
+porcentCoronavac = (quantCoronavac * 100) / totalPessoas
+# calcula a porcentagem de pessoas que foram vacinadas com Astrazeneca
+porcentAstrazeneca = (quantAstrazeneca * 100) / totalPessoas
+# calcula a porcentagem de pessoas que foram vacinadas com Pfizer
+porcentPfizer = (quantPfizer * 100) / totalPessoas
+# calcula a porcentagem de pessoas que foram vacinadas com janssen
+porcentJanssen = (quantJanssen * 100) / totalPessoas
+
+# saída
+print(f"porcentagem de pessoas que foram vacinadas: {porcentagemVacinados}%")
+print(f"total de vacinados: {quantVacinados}")
+print(f"A porcentagem de pessoas que foram vacinadas com Coronavac é {porcentCoronavac}%")
+print(f"A porcentagem de pessoas que foram vacinadas com Astrazeneca é {porcentAstrazeneca}%")
+print(f"A porcentagem de pessoas que foram vacinadas com Pfizer é {porcentPfizer}%")
+print(f"A porcentagem de pessoas que foram vacinadas com janssen é {porcentJanssen}%")
+
+# ritmo
+if (porcentagemVacinados >= 0 and porcentagemVacinados < 15):
+    print("Ritmo: Lento")
+elif (porcentagemVacinados >= 15 and porcentagemVacinados < 40):
+    print("Ritmo: Mediano")
+elif (porcentagemVacinados >= 40 and porcentagemVacinados < 60):
+    print("Ritmo: Bom")
+elif (porcentagemVacinados >= 60 and porcentagemVacinados <= 100):
+    print("Ritmo: Acelerado")
+
+print("Fim do programa")
